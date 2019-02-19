546db006766947fdd236c58958dc8f548ed0b7b4
Kanishka <bandarukanishka@gmail.com>
2019-02-19 11:33:11 +0530

    Test commit
    Signed-off-by: Kanishka <bandarukanishka@gmail.com>

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -8,6 +8,8 @@ import com.soulgalore.jdbcmetrics.*;
 import org.slf4j.*;
 
 
+
+
 public class JDBC {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:jdbcmetrics:mysql://localhost:3306/kaka?jdbcmetrics=yes";
